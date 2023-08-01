258936130cc4ff17657464c39d7d1ab7d9ce8a65
jesusfb <jfigueroa.beltran@gmail.com>
2023-07-31 19:43:18 -0700

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ const MONGO_IP = process.env.MONGO_IP;
 const MONGO_PORT = process.env.MONGO_PORT;
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
-const MONGO_URL = `mongodb://${MONGO_IP}/?authSource=admin`;
+const MONGO_URL = `mongodb+srv://jesusfb:Dove3229-@cluster0.yx9sjqo.mongodb.net/?authSource=admin`;
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 4000;
 
